575208b13a45af246a2b2cfb0ecadeaf247eb8e0
lucasfarmwald <farmwaldlucas@gmail.com>
2021-08-12 22:45:44 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Hannah Farmwald';
+const welcomeMessage = "Welcome to Flatbook, ${Hannah Farmwald}!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
